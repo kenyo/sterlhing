3fce88c3f8ee52dae995f805df3271bd77e0a5ee
Kenny Oh <kennyoh517@gmail.com>
2020-12-05 12:28:56 -0500

    add compareTriplets

--- coding-challenge/find-second-maximum-number-in-a-list.py
+++ coding-challenge/find-second-maximum-number-in-a-list.py
@@ -13,3 +13,16 @@ for x in arr:
     second_max = x
 
 print(second_max)
+
+
+# Complete the compareTriplets function below.
+def compareTriplets(a, b):
+    a_score = 0
+    b_score = 0
+    for i in range(3):
+        if(a[i] > b[i]):
+            a_score += 1
+        elif(a[i] < b[i]):
+            b_score += 1
+        
+    return (a_score, b_score)
