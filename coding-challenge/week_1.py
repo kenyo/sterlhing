9e27735fd5ab9e4d8b336dddcdd8fec277877f4a
Kenny Oh <kennyoh517@gmail.com>
2020-12-05 12:33:14 -0500

    add compareTriplets

--- coding-challenge/week_1.py
+++ coding-challenge/week_1.py
@@ -26,3 +26,16 @@ def compareTriplets(a, b):
             b_score += 1
         
     return (a_score, b_score)
+
+  
+# Complete the compareTriplets function below.
+def compareTriplets(a, b):
+    a_score = 0
+    b_score = 0
+    for i in range(3):
+        if(a[i] > b[i]):
+            a_score += 1
+        elif(a[i] < b[i]):
+            b_score += 1
+        
+    return (a_score, b_score)
