5335b934b491ffbeb5613422a649042d3f63ccc4
Kenny Oh <kennyoh517@gmail.com>
2020-12-05 12:34:19 -0500

    add aVeryBigSum

--- coding-challenge/week_1.py
+++ coding-challenge/week_1.py
@@ -28,14 +28,13 @@ def compareTriplets(a, b):
     return (a_score, b_score)
 
   
-# Complete the compareTriplets function below.
-def compareTriplets(a, b):
-    a_score = 0
-    b_score = 0
-    for i in range(3):
-        if(a[i] > b[i]):
-            a_score += 1
-        elif(a[i] < b[i]):
-            b_score += 1
-        
-    return (a_score, b_score)
+import math
+import os
+import random
+import re
+import sys
+import functools
+
+# Complete the aVeryBigSum function below.
+def aVeryBigSum(ar):
+    return functools.reduce(lambda x, y: x + y, ar)
