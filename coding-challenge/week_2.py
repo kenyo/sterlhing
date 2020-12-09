d18230c9692fd2756c019c1950f74eb35b06a3c6
Kenny Oh <kennyoh517@gmail.com>
2020-12-09 15:20:00 -0500

    add plusMinus

---
+++ coding-challenge/week_2.py
@@ -0,0 +1,17 @@
+def plusMinus(arr):
+    arr_len = len(arr)
+    plus = 0
+    minus = 0
+    zero = 0
+    
+    for x in arr:
+        if(x > 0):
+            plus += 1
+        elif(x < 0):
+            minus += 1
+        else:
+            zero += 1 
+    
+    print("{0:.6f}".format(plus/arr_len))
+    print("{0:.6f}".format(minus/arr_len))
+    print("{0:.6f}".format(zero/arr_len))
