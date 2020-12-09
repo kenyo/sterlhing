daf0802711a2a4d1dacfdccfcaad7d1fc0a111ca
Kenny Oh <kennyoh517@gmail.com>
2020-12-09 15:36:03 -0500

    add staircase

--- coding-challenge/week_2.py
+++ coding-challenge/week_2.py
@@ -15,3 +15,7 @@ def plusMinus(arr):
     print("{0:.6f}".format(plus/arr_len))
     print("{0:.6f}".format(minus/arr_len))
     print("{0:.6f}".format(zero/arr_len))
+
+def staircase(n):
+    for x in range(n):
+        print(" " * (n-x-1) + "#" * (x+1))
