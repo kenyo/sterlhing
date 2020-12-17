07d7d0d2bd24e1dfdaa690cfe9fccc2237b8a5dc
Kenny Oh <kennyoh517@gmail.com>
2020-12-17 09:55:28 -0500

    add nestedList

--- coding-challenge/week_3.py
+++ coding-challenge/week_3.py
@@ -10,3 +10,39 @@ if __name__ == '__main__':
     query_name = input()
     
     print ("{:0.2f}".format(reduce(lambda x, y: x + y, student_marks[query_name]) / len(student_marks[query_name])))
+
+    
+if __name__ == '__main__':
+    # function to get unique values 
+    def unique(list1): 
+      
+        # intilize a null list 
+        unique_list = [] 
+          
+        # traverse for all elements 
+        for x in list1: 
+            # check if exists in unique_list or not 
+            if x not in unique_list: 
+                unique_list.append(x)
+        
+        return unique_list
+      
+    nested_list = []
+    for _ in range(int(input())):
+        name = input()
+        score = float(input())
+        
+        nested_list.append([name, score])
+    
+    nested_list.sort(key = lambda x: x[1])
+    scores = unique(map(lambda x: x[1], nested_list))
+    second_lowest_score = scores[1]
+    
+    second_lowest_students = []
+    for pair in nested_list:
+      if pair[1] == second_lowest_score:
+        second_lowest_students.append(pair[0])
+    
+    second_lowest_students.sort()
+    for s in second_lowest_students:
+      print(s)    
