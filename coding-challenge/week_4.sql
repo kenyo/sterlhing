f1f4a516ec67323b5b23afb63d06e57e0c8d6897
kenny oh <kennyoh517@gmail.com>
2020-12-22 21:42:07 -0500

    add sql query

--- coding-challenge/week_4.sql
+++ coding-challenge/week_4.sql
@@ -1,2 +1,6 @@
+-- chain logical expressions with `and`
 SELECT name from city
 WHERE Population > 120000 AND CountryCode = 'USA';
+
+-- use `distinct` keyword to get unique values
+SELECT DISTINCT city FROM station WHERE id % 2 = 0;
