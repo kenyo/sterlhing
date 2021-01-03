d27e7a9a3664483833eda74139fe9e77755d1f35
kenny oh <kennyoh517@gmail.com>
2021-01-03 18:48:37 -0500

    add rotLeft

---
+++ coding-challenge/week_5.js
@@ -0,0 +1,11 @@
+function rotLeft(a, d) {
+  const rotatedArray = new Array(a.length)
+
+  a.forEach((x, i) => {
+    const newIndex = i - d
+    rotatedArray.splice(newIndex, 1, x)
+  })
+
+  return rotatedArray
+
+}
