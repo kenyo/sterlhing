cbd5480f526d0c8da2da1a50399187664fc4b6e6
kenny oh <kennyoh517@gmail.com>
2021-01-03 19:46:41 -0500

    add city length query

--- coding-challenge/week_5.sql
+++ coding-challenge/week_5.sql
@@ -1,2 +1,12 @@
 SELECT COUNT(CITY) - COUNT(DISTINCT(CITY))
 FROM STATION ;
+
+select city c, length(city) l
+from   station
+order by l desc, c asc
+limit 1;
+
+select city c, length(city) l
+from   station
+order by l asc, c asc
+limit 1;
