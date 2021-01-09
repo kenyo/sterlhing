fa30ad0295d566a5afd6df247317c45b540dd3a4
kenny oh <kennyoh517@gmail.com>
2021-01-09 16:33:11 -0500

    add vowel cities

--- coding-challenge/week_6.sql
+++ coding-challenge/week_6.sql
@@ -7,3 +7,8 @@ select city c, length(city) l
 from   station
 order by l asc, c asc
 limit 1;
+
+
+SELECT CITY
+FROM STATION
+WHERE CITY Like 'A%' OR  CITY LIKE 'E%' OR CITY LIKE 'I%' OR CITY LIKE 'O%' OR CITY LIKE 'U%';
