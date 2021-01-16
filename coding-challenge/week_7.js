da934a1bfe6f52b4c5eb995da5dd0253a4cef6ff
kenny oh <kennyoh517@gmail.com>
2021-01-16 17:07:24 -0500

    add persistence

--- coding-challenge/week_7.js
+++ coding-challenge/week_7.js
@@ -1,3 +1,18 @@
 function openOrSenior(data){
   return data.map(([age, handicap]) => (age > 54 && handicap > 7) ? 'Senior' : 'Open');
 }
+
+
+function persistence(num) {
+  let counter = 0
+
+  let persistentNum = num
+  while (persistentNum.toString().length > 1) {
+    const strNum = persistentNum.toString()
+    persistentNum = strNum.split('').reduce((x, y) => x * y)
+
+    counter++
+  }
+
+  return counter
+}
