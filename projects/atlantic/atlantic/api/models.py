02ecb88593d374d6405d4f854431a329da7e3d41
kenny oh <kennyoh517@gmail.com>
2021-01-31 16:42:04 -0500

    add migrations

--- projects/atlantic/atlantic/api/models.py
+++ projects/atlantic/atlantic/api/models.py
@@ -1,7 +1,5 @@
 from django.db import models
 
-# models.IntegerField()
-
 # Create your models here.
 class Customer(models.Model):
   id = models.CharField(max_length=100, primary_key=True)
@@ -13,7 +11,15 @@ class Customer(models.Model):
   state = models.CharField(max_length=2)
   postal_code = models.CharField(max_length=9)
 
-# class Subscription(models.Model):
+class Subscription(models.Model):
+  id = models.CharField(max_length=100, primary_key=True)
+  customer_id = models.ForeignKey(Customer, on_delete=models.CASCADE)
+  plan_name = models.CharField(max_length=100)
+  price = models.IntegerField()
 
-
-# class Gifts(models.Model):
+class Gifts(models.Model):
+  id = models.CharField(max_length=100, primary_key=True)
+  customer_id = models.ForeignKey(Customer, on_delete=models.CASCADE)
+  plan_name = models.CharField(max_length=100)
+  price = models.IntegerField()
+  recipient_email = models.CharField(max_length=100)
--- projects/atlantic/atlantic/api/views.py
+++ projects/atlantic/atlantic/api/views.py
@@ -4,7 +4,7 @@ from django.views.decorators.csrf import csrf_exempt
 
 import json
 
-from api.models import Customer
+from api.models import Customer, Subscription, Gifts
 
 def home(request):
     return HttpResponse('Hello, World!')
@@ -14,13 +14,40 @@ def home(request):
 # create or update customer from webhook
 def crupdate_customer(request):
   if request.method == 'POST':
+
+
     payload = json.loads(request.body)
     customer = payload['customer']
+    customer_id =  customer['id']
 
-    # #     car = Car(name=car_name, top_speed=top_speed)
-    # #     try:
-    # #         car.save()
-    response = json.dumps([{ 'Success': 'Car added successfully!'}])
+    person, created = Customer.objects.get_or_create(id=customer_id)
+
+    if created:
+      print('foo')
+      # means you have created a new person
+    else:
+      print('bar')
+      # person just refers to the existing one
+
+    # check if customer exists
+    # try:
+    #   existing_customer = Customer.objects.get(id=customer_id)
+    #   if existing_customer:
+    #     # update customer
+
+    #   else:
+    #     new_customer = Customer(**customer)
+    #     new_customer.save()
+    # # #     car = Car(name=car_name, top_speed=top_speed)
+    # # #     try:
+    # # #         car.save()
+    #   response = json.dumps([{ 'Success': 'Car added successfully!'}])
+    # except:
+    #   response = json.dumps([{ 'Failure': 'Problems'}])
+
+
+
+
     # #     except:
     # #         response = json.dumps([{ 'Error': 'Car could not be added!'}])
   return HttpResponse(response, content_type='text/json')
