cbe96cd886fd7048be83904867deb0d19edc2474
kenny oh <kennyoh517@gmail.com>
2021-02-01 10:56:43 -0500

    add create or update customer

--- projects/atlantic/atlantic/api/views.py
+++ projects/atlantic/atlantic/api/views.py
@@ -18,36 +18,24 @@ def crupdate_customer(request):
 
     payload = json.loads(request.body)
     customer = payload['customer']
-    customer_id =  customer['id']
+    customer_id = customer['id']
+    # customer['subscription'].update({'customer_id': customer_id})
+    subscription = customer['subscription']
+    # gifts = map(lambda x: x.update({'customer_id': customer_id}), customer['gifts'])
+    gifts = customer['gifts']
 
-    person, created = Customer.objects.get_or_create(id=customer_id)
+    try:
+      customer, created = Customer.objects.update_or_create(id=customer_id, defaults=customer)
+      subscription, sub_created = Subscription.objects.update_or_create(id=subscription['id'], customer_id=customer, defaults=subscription)
 
-    if created:
-      print('foo')
-      # means you have created a new person
-    else:
-      print('bar')
-      # person just refers to the existing one
+      for gift in gifts:
+        Gifts.objects.update_or_create(customer_id=customer, defaults=gift)
 
-    # check if customer exists
-    # try:
-    #   existing_customer = Customer.objects.get(id=customer_id)
-    #   if existing_customer:
-    #     # update customer
+      if created:
+        response = json.dumps([{ 'Success': 'Customer successfully created.'}])
+      else:
+        response = json.dumps([{ 'Success': 'Customer successfully updated.'}])
+    except:
+      response = json.dumps([{ 'Failure': 'Problem saving new customer.' }])
 
-    #   else:
-    #     new_customer = Customer(**customer)
-    #     new_customer.save()
-    # # #     car = Car(name=car_name, top_speed=top_speed)
-    # # #     try:
-    # # #         car.save()
-    #   response = json.dumps([{ 'Success': 'Car added successfully!'}])
-    # except:
-    #   response = json.dumps([{ 'Failure': 'Problems'}])
-
-
-
-
-    # #     except:
-    # #         response = json.dumps([{ 'Error': 'Car could not be added!'}])
   return HttpResponse(response, content_type='text/json')
