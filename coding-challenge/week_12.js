caa5506960a7919237f1cf0ffaf95d60e3b7235e
kenny oh <kennyoh517@gmail.com>
2021-02-21 18:40:27 -0500

    add countSwaps

---
+++ coding-challenge/week_12.js
@@ -0,0 +1,27 @@
+function countSwaps(a) {
+    let sortCount = 0;
+    for (let i = 0; i < a.length; i++) {
+
+        for (let j = 0; j < a.length - 1; j++) {
+
+            if (a[j] > a[j + 1]) {
+                let temp = a[j];
+                a[j] = a[j + 1];
+                a[j + 1] = temp;
+                sortCount += 1;
+            }
+        }
+
+    }
+
+    console.log('Array is sorted in', sortCount, 'swaps.');
+    console.log('First Element:', a[0]);
+    console.log('Last Element:', a[a.length - 1]);
+
+}
+
+
+
+
+
+
